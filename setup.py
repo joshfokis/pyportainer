47030f771a1933271bcb3753b98b3091a1ef05f5
Josh Flinner <joshfokis@gmail.com>
2020-04-21 01:08:15 -0500

    format and convert concat strings to f strings

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyPortainer',
-    version='0.1dev',
+    version='0.2',
     packages=['pyportainer',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('pyportainer/README.md').read(),
